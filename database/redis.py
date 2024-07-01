c769e273e05cda1809c9f6312860708e60c9acfe
Nikita Garashchuk <ngarashchuk03@internet.ru>
2024-07-01 10:39:36 +0300

    Update redis.py

--- database/redis.py
+++ database/redis.py
@@ -4,7 +4,7 @@ from config import REDIS_HOST, REDIS_PORT
 from aiogram.fsm.storage.redis  import RedisStorage 
 
 # Создание подключения к Redis
-#redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 # Инициализация хранилища состояний для aiogram
-#storage = RedisStorage(redis)
+storage = RedisStorage(redis)
